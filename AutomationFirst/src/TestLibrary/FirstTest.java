9982a23ce045734744d3e0dd97430b969a5a4902
shubhamfrank <shubhamfrank@gmail.com>
2021-10-23 15:14:05 +0530

    First Upload

---
+++ AutomationFirst/src/TestLibrary/FirstTest.java
@@ -0,0 +1,18 @@
+package TestLibrary;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.gecko.driver", "D:\\Testing\\Automation\\Selenium\\geckodriver\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.com");
+		driver.quit();
+	}
+
+}
